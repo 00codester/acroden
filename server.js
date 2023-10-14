cc6b6ec48fdef6b7047d9dba1b440471fc2c434c
00codester <00codester@gmail.com>
2023-10-14 16:17:05 -0600

    added procfile and cors

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongodb = require('./db/connections');
 const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
 app
+    .use(cors())
     .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
     .use('/', require('./routes'));
 
 app.listen(port, () => {
