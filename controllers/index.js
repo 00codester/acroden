9461ef26313a47b877f5db14f058f94fcfebe337
00codester <00codester@gmail.com>
2023-10-14 15:38:16 -0600

    add mongodb and dotenv to package

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,6 @@
+const mongodb = require('../db/connections.js');
+const ObjectId = require('mongodb').ObjectId;
+
 const awesomeFunction = (req, res, next) => {
     res.json('Emily Karl');
 };
@@ -6,7 +9,20 @@ const superAwesomeFunction = (req, res, next) => {
     res.json('Super Awesome Person');
 };
 
+const getAll = async(req, res) => {
+    try {
+        const result = await mongodb.getDb().db().collection('clients').find();
+        result.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json')
+            res.status(200).json(lists);
+        });
+    } catch(err){
+        res.status(500).json({ message: err.message });
+    }
+};
+
 module.exports = {
     awesomeFunction,
-    superAwesomeFunction
+    superAwesomeFunction,
+    getAll
 };
