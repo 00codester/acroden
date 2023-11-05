80de7442d3bdefb3bdc86f74ccfcfbca3d4f5e04
00codester <00codester@gmail.com>
2023-11-04 22:28:22 -0600

    updated inventory put route

--- routes/inventory.js
+++ routes/inventory.js
@@ -7,7 +7,7 @@ const validation = require('../middleware/validate');
 routes.get('/', controllerFile.getAllForSale);
 routes.get('/:id', validation.idLengthCheck, controllerFile.getSingleItemForSale);
 routes.post('/', validation.saveItem, controllerFile.addNewItemForSale);
-routes.put('/:id', validation.saveItem, controllerFile.updateInventoryItem);
+routes.put('/', validation.saveItem, controllerFile.updateInventoryItem);
 routes.delete('/:id',validation.idLengthCheck, controllerFile.deleteInventoryItem);
 
 
